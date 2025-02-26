006e4a2a32b3b306cc9123de383cc1765316e1f7
DinaGhoneim <dina90736@gmail.com>
2025-02-26 23:06:43 +0200

    26/2/2025

--- lib/presentation/views/student_affairs/application.dart
+++ lib/presentation/views/student_affairs/application.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class ApplicationScreen extends StatelessWidget {
   const ApplicationScreen({super.key});
 
-  @override
+  @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
